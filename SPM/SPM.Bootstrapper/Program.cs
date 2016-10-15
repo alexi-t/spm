261960b831ec936b153831072c94feab1c55f864
canki <cankrus@gmail.com>
2016-10-16 05:31:02 +0800

    bootstrapper and shell projects

---
+++ SPM/SPM.Bootstrapper/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPM.Bootstrapper
+{
+    static class Program
+    {
+        const string spmAppFolder = "SPM";
+        const string spmExecutableName = "SPM.Shell";
+
+        [LoaderOptimization(LoaderOptimization.MultiDomainHost)]
+        [STAThread]
+        static void Main(params string[] args)
+        {
+            var localAppFolder = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+
+            var appDirectory = Directory.CreateDirectory(Path.Combine(localAppFolder, "SPM"));
+
+            var cachePath = Path.Combine(appDirectory.FullName, "assemblyCache");
+            var config = Path.Combine(appDirectory.FullName, "App", $"{spmExecutableName}.exe.config");
+            var executable = Path.Combine(appDirectory.FullName, "App", $"{spmExecutableName}.exe");
+
+            AppDomainSetup setup = new AppDomainSetup
+            {
+                ApplicationName = "SPM",
+                ShadowCopyFiles = "true",
+                CachePath = cachePath,
+                ConfigurationFile = config
+            };
+
+            AppDomain domain = AppDomain.CreateDomain(
+                "SPM",
+                AppDomain.CurrentDomain.Evidence,
+                setup);
+
+            domain.ExecuteAssembly(executable, args);
+
+            AppDomain.Unload(domain);
+            Directory.Delete(cachePath, true);
+        }
+    }
+}
---
+++ SPM/SPM.Shell/SPM.cs
@@ -0,0 +1,28 @@
+﻿using CLAP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPM.Shell
+{
+    public static class SPM
+    {
+
+        public static int Main(params string[] args)
+        {
+            CLAP.Parser.Run<Commands>(args);
+            return 0;
+        }
+
+        public class Commands
+        {
+            [Verb(Aliases = "update-self")]
+            public static void UpdateSelf()
+            {
+                Console.WriteLine("Update");
+            }
+        }
+    }
+}
