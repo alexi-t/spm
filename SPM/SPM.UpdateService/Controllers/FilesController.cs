2fad93b6610ee08cbc812cc740f17f8b19312d7e
canki <cankrus@gmail.com>
2016-10-22 05:59:41 +0800

    fix post build

---
+++ SPM/SPM.UpdateService/Controllers/FilesController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SPM.UpdateService.Services;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+
+namespace SPM.UpdateService.Controllers
+{
+    [Route("api/[controller]")]
+    public class FilesController : Controller
+    {
+        private readonly VersionsService versionService;
+
+        private readonly IHostingEnvironment _environment;
+        private readonly IHttpContextAccessor contextAccessor;
+
+        public FilesController(IHostingEnvironment environment, VersionsService versionService, IHttpContextAccessor contextAccessor)
+        {
+            _environment = environment;
+            this.versionService = versionService;
+            this.contextAccessor = contextAccessor;
+        }
+
+        [HttpGet]
+        [Route("getLastVersion")]
+        public async Task<IActionResult> GetLastVersion()
+        {
+            return File(await versionService.GetLastVersionFileStream(), "application/zip");
+        }
+
+        [HttpPost]
+        public void Post(IFormFile file)
+        {
+            using (var sr = new BinaryReader(file.OpenReadStream()))
+            {
+                var data = sr.ReadBytes((int)sr.BaseStream.Length);
+
+                var nextVersion = versionService.GetNextAvailableVersion();
+                versionService.AddNewVersion(nextVersion, data);
+            }
+            
+        }
+
+    }
+}
---
+++ SPM/SPM.UpdateService/Services/VersionsService.cs
@@ -0,0 +1,91 @@
+﻿using SPM.UpdateService.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TechSmith.Hyde;
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System.IO;
+
+namespace SPM.UpdateService.Services
+{
+    public class VersionsService
+    {
+        string tableName = "appversions";
+        string partiionName = "Versions";
+
+        string connectionString = "DefaultEndpointsProtocol=https;AccountName=spmspu;AccountKey=OQkq6rH6vVdV2BGU6LaFLpRWsdkwANrdR/hvKZ1Hmd3iVnZYMnbn6Bxxa06kADC3Iou/RKR4/nw4MbjGcY/9rw==";
+        
+        public string GetNextAvailableVersion()
+        {
+            TechSmith.Hyde.Table.AzureTableStorageProvider storage = GetStorage();
+
+            var lastEntity = storage.CreateQuery<VersionEntity>(tableName).PartitionKeyEquals(partiionName).Take(1).FirstOrDefault();
+
+            if (lastEntity == null)
+                return "0.0.1.0";
+
+            if (!string.IsNullOrEmpty(lastEntity.Version))
+            {
+                var version = new Version(lastEntity.Version);
+
+                version = new Version(version.Major, version.Minor, version.Build, version.Revision + 1);
+
+                return version.ToString();
+            }
+
+            return null;
+        }
+
+        internal async Task<byte[]> GetLastVersionFileStream()
+        {
+            TechSmith.Hyde.Table.AzureTableStorageProvider storage = GetStorage();
+
+            var lastEntity = storage.CreateQuery<VersionEntity>(tableName).PartitionKeyEquals(partiionName).Take(1).FirstOrDefault();
+
+            if (lastEntity == null)
+                return null;
+
+            var blob = GetVersionBlob(lastEntity.Version);
+
+            using (var ms = new MemoryStream())
+            {
+                await blob.DownloadToStreamAsync(ms);
+                return ms.ToArray();
+            }
+        }
+
+        private TechSmith.Hyde.Table.AzureTableStorageProvider GetStorage()
+        {
+            var account = new ConnectionStringCloudStorageAccount(connectionString);
+            var storage = new TechSmith.Hyde.Table.AzureTableStorageProvider(account);
+            return storage;
+        }
+
+        public string AddNewVersion(string version, byte[] data)
+        {
+            TechSmith.Hyde.Table.AzureTableStorageProvider storage = GetStorage();
+            storage.Add(tableName, new VersionEntity(version));
+            storage.Save();
+
+            CloudBlockBlob blob = GetVersionBlob(version);
+
+            blob.UploadFromByteArray(data, 0, data.Length);
+
+            return blob.Uri.ToString();
+        }
+
+        private CloudBlockBlob GetVersionBlob(string version)
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connectionString);
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            var container = blobClient.GetContainerReference("updates");
+            container.CreateIfNotExists();
+            var blob = container.GetBlockBlobReference("update-" + version + ".zip");
+            return blob;
+        }
+    }
+}
