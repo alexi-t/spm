d685c50a55bdf43f0ba3dbcb8a2bacee86054c2d
canki <cankrus@gmail.com>
2016-10-23 03:32:37 +0800

    remove build folder, add install script

---
+++ scripts/install.ps1
@@ -0,0 +1,29 @@
+﻿$release = "https://github.com/icanki/spm/files/545961/spm.zip"
+$localAppData = $env:LOCALAPPDATA
+
+Write-Host "Creating directory..."
+$appFolder = [System.IO.Directory]::CreateDirectory([System.IO.Path]::Combine($localAppData,"SPM"))
+
+Get-ChildItem $appFolder.FullName | Remove-Item -Recurse -Force
+
+Write-Host "Downloading bootstrapper..." -NoNewline
+
+$wc= New-Object System.Net.WebClient
+$zip = [System.IO.Path]::Combine($appFolder.FullName, "spm.zip")
+$wc.DownloadFile($release, $zip)
+
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+[System.IO.Compression.ZipFile]::ExtractToDirectory($zip, $appFolder.FullName)
+
+Write-Host "Done" -ForegroundColor Green
+
+Write-Host "Download app..." -NoNewline
+$zip = [System.IO.Path]::Combine($appFolder.FullName, "app.zip")
+$wc.DownloadFile("http://spmservices.azurewebsites.net/api/files/getLastVersion", $zip)
+$appShellFolder = [System.IO.Directory]::CreateDirectory([System.IO.Path]::Combine($appFolder.FullName,"App"))
+[System.IO.Compression.ZipFile]::ExtractToDirectory($zip, $appShellFolder.FullName)
+Write-Host "Done" -ForegroundColor Green
+
+Write-Host "Write Path..." -NoNewline
+SETX Path $env:Path+';'+$appFolder.FullName > void
+Write-Host "Done" -ForegroundColor Green
