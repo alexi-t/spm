65f4bafc237df544c4ef40f61321add8d6d63b0e
canki <cankrus@gmail.com>
2016-11-26 17:47:40 +0800

    pull command

---
+++ src/SPM/SPM.Shell/Commands/Pull/NewPackagePack.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPM.Shell.Commands.Pull
+{
+    public class NewPackage
+    {
+        public string Version { get; set; }
+
+        public string Name { get; set; }
+
+        public string PackageUrl { get; set; }
+    }
+}
---
+++ src/SPM/SPM.Shell/Commands/Pull/PullCommandParser.cs
@@ -0,0 +1,46 @@
+﻿using CLAP;
+using SPM.Shell.Config;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPM.Shell.Commands.Pull
+{
+    public class PullCommandParser
+    {
+        private readonly IPullService pullService;
+
+        public PullCommandParser(IPullService pullService)
+        {
+            this.pullService = pullService;
+        }
+
+        [Verb()]
+        public async void Pull()
+        {
+            var config = ConfigManager.ReadFromWorkingDirectory();
+
+            foreach (var package in config.Packages.Values)
+            {
+                var newVersion = pullService.Pull(package.Name);
+                if (newVersion.Version != package.Version)
+                {
+                    package.Version = newVersion.Version;
+
+                    var httpClient = new HttpClient();
+
+                    var newPackageRequest = await httpClient.GetAsync(newVersion.PackageUrl);
+
+                    var data = await newPackageRequest.Content.ReadAsByteArrayAsync();
+                    File.WriteAllBytes(package.Name, data);
+
+                    ConfigManager.UpdatePackageConfig(package.Name, package);
+                }
+            }
+        }
+    }
+}
