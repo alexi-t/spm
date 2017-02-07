b4928a39e3365034f419c39c1f144d10ad8a9bb8
canki <cankrus@gmail.com>
2017-02-08 02:01:20 +0800

    remove old projects cloud storage implementation for files service

--- src/SPM/SPM.Http.FileService/Startup.cs
+++ src/SPM/SPM.Http.FileService/Startup.cs
@@ -27,6 +27,7 @@ namespace SPM.Http.FileService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient(c => new Services.CloudBlobService(Configuration["AzureStorageKey"]));
             // Add framework services.
             services.AddMvc();
         }
