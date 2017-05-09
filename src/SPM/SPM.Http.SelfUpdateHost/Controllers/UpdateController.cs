28643064415d2cf6457688f0f18df62600db0b0b
canki <cankrus@gmail.com>
2017-05-10 00:43:36 +0800

    add self update host

---
+++ src/SPM/SPM.Http.SelfUpdateHost/Controllers/UpdateController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Http;
+
+namespace SPM.Http.SelfUpdateHost.Controllers
+{
+    [Route("api/[controller]")]
+    public class UpdateController : Controller
+    {
+        private readonly Config config;
+
+        public UpdateController(IOptions<Config> configOptions)
+        {
+            config = configOptions.Value;
+        }
+
+        // GET api/values
+        [HttpGet("getLastVersion")]
+        public string GetLastVersion()
+        {
+            return string.Empty;
+        }
+        
+        // PUT api/values/5
+        [HttpPut("{fileName}")]
+        public IActionResult Put(string version, IFormFile zip, [FromHeader(Name = "X-ServerDigest")]string digest)
+        {
+            if (digest != config.ServerDigest)
+                return Unauthorized();
+
+            if (zip == null || string.IsNullOrEmpty(version))
+                return BadRequest();
+
+            return Ok();
+        }
+    }
+}
